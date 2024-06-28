8cac33ab3d7cb55209bd41f844acb3829438341e
alaasao <a_souchi@estin.dz>
2024-06-28 08:51:57 +0100

    url

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -34,7 +34,9 @@ const Login = () => {
     e.stopPropagation();
 
     const URL = `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/auth/login`;
-
+    console.log("**************")
+    console.log(URL)
+    console.log("**************")
     axios.post(URL, data, {
         withCredentials: true,
         headers: {
